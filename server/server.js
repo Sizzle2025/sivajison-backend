874a53795744f8bb5867e7b2a11d7e6a7a176a9c
Sizzle2025 <sarathibalaji2037@gmail.com>
2025-06-24 16:20:42 +0530

    gj commit

--- server/server.js
+++ server/server.js
@@ -7,10 +7,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+console.log("Connecting to MongoDB:", process.env.MONGO_URI)
 
 
 // ✅ Connect to MongoDB
-mongoose.connect("process.env.MONGO_URI/visaDB", {
+mongoose.connect( process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
